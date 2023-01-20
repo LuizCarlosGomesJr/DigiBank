c88066ee425c6d2926d3851a533e720165ff67e0
Luiz Gomes <72467078+LuizCarlosGomesJr@users.noreply.github.com>
2023-01-20 01:09:20 -0300

    Add files via upload

---
+++ Contratos/IConta.cs
@@ -0,0 +1,18 @@
+﻿using DigiBank.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigiBank.Contratos
+{
+    public interface IConta
+    {
+        void Depositar(double valor);
+        bool Sacar(double valor);
+        double ConsultaSaldo();
+        string GetCodigoDoBanco();
+        string GetNumeroDaAgencia();
+        string GetNumeroDaConta();
+        List<Extrato> Extrato();
+    }
+}
